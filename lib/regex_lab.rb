cd407a59b2458a814cdb984f845e20ebb0a0444c
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-11 15:41:53 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  numbers_valid = phone.scan
   if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
     true
   else
